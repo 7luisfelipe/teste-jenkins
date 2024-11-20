6c754c6ee7e831d998a2097e783800a4b6591158
Luis Felipe <luis14_felipe@hotmail.com>
2024-11-20 16:36:45 -0300

    commit inicial

---
+++ src/main/java/br/com/lf/testjenkins/controller/HelloController.java
@@ -0,0 +1,22 @@
+package br.com.lf.testjenkins.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/v1/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<String> getHello(){
+        return new ResponseEntity<>(
+                "Successo",
+                HttpStatus.OK
+        );
+    }
+
+}
